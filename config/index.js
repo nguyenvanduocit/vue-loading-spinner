673d52b47b2efe9e0ea4d3e1bd460b3eae60459b
nguyenvanduocit <nguyenvanduocit@gmail.com>
2016-12-20 06:09:58 -0800

    remove config directory

--- config/index.js
+++
@@ -1,9 +0,0 @@
-// see http://vuejs-templates.github.io/webpack for documentation.
-var path = require('path')
-
-module.exports = {
-  build: {
-    assetsRoot: path.resolve(__dirname, '../dist'),
-    productionSourceMap: true
-  }
-}
