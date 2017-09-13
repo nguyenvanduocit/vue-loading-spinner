332af79abd217dbdfbdbe140cba81e4a1eeaf77d
Duoc <duoc@construct.sg>
2017-09-13 23:15:53 +0700

    simplize index export

--- src/index.js
+++ src/index.js
@@ -1,40 +1,83 @@
-export {Circle} from './components/Circle.vue'
-export {Circle2} from './components/Circle2.vue'
-export {Circle3} from './components/Circle3.vue'
-export {Circle4} from './components/Circle4.vue'
-export {Circle5} from './components/Circle5.vue'
-export {Circle6} from './components/Circle6.vue'
-export {Circle7} from './components/Circle7.vue'
-export {Circle8} from './components/Circle8.vue'
-export {Circle9} from './components/Circle9.vue'
-export {Circle10} from './components/Circle10.vue'
-export {Circle11} from './components/Circle11.vue'
-export {CubeShadow} from './components/CubeShadow.vue'
-export {Diamond} from './components/Diamond.vue'
-export {DoubleBounce} from './components/DoubleBounce.vue'
-export {Gauge} from './components/Gauge.vue'
-export {Google} from './components/Google.vue'
-export {Hexagon} from './components/Hexagon.vue'
-export {HourGlass} from './components/HourGlass.vue'
-export {Jawn} from './components/Jawn.vue'
-export {Jumper} from './components/Jumper.vue'
-export {LetterCube} from './components/LetterCube.vue'
-export {MikePad} from './components/Mikepad.vue'
-export {Origami} from './components/Origami.vue'
-export {Pencil} from './components/Pencil.vue'
-export {PingPong} from './components/PingPong.vue'
-export {Plane} from './components/Plane.vue'
-export {RotateSquare} from './components/RotateSquare.vue'
-export {RotateSquare2} from './components/RotateSquare2.vue'
-export {RotateSquare3} from './components/RotateSquare3.vue'
-export {RotateSquare4} from './components/RotateSquare4.vue'
-export {RotateSquare5} from './components/RotateSquare5.vue'
-export {ScaleOut} from './components/ScaleOut.vue'
-export {Socket} from './components/Socket.vue'
-export {SpinLine} from './components/SpinLine.vue'
-export {SquareGrid} from './components/SquareGrid.vue'
-export {Stretch} from './components/Stretch.vue'
-export {Texture} from './components/Texture.vue'
-export {ThreeDots} from './components/ThreeDots.vue'
-export {TwoCube} from './components/TwoCube.vue'
-export {Wave} from './components/Wave.vue'
+import Circle from './components/Circle.vue'
+import Circle2 from './components/Circle2.vue'
+import Circle3 from './components/Circle3.vue'
+import Circle4 from './components/Circle4.vue'
+import Circle5 from './components/Circle5.vue'
+import Circle6 from './components/Circle6.vue'
+import Circle7 from './components/Circle7.vue'
+import Circle8 from './components/Circle8.vue'
+import Circle9 from './components/Circle9.vue'
+import Circle10 from './components/Circle10.vue'
+import Circle11 from './components/Circle11.vue'
+import CubeShadow from './components/CubeShadow.vue'
+import Diamond from './components/Diamond.vue'
+import DoubleBounce from './components/DoubleBounce.vue'
+import Gauge from './components/Gauge.vue'
+import Google from './components/Google.vue'
+import Hexagon from './components/Hexagon.vue'
+import HourGlass from './components/HourGlass.vue'
+import Jawn from './components/Jawn.vue'
+import Jumper from './components/Jumper.vue'
+import LetterCube from './components/LetterCube.vue'
+import MikePad from './components/Mikepad.vue'
+import Origami from './components/Origami.vue'
+import Pencil from './components/Pencil.vue'
+import PingPong from './components/PingPong.vue'
+import Plane from './components/Plane.vue'
+import RotateSquare from './components/RotateSquare.vue'
+import RotateSquare2 from './components/RotateSquare2.vue'
+import RotateSquare3 from './components/RotateSquare3.vue'
+import RotateSquare4 from './components/RotateSquare4.vue'
+import RotateSquare5 from './components/RotateSquare5.vue'
+import ScaleOut from './components/ScaleOut.vue'
+import Socket from './components/Socket.vue'
+import SpinLine from './components/SpinLine.vue'
+import SquareGrid from './components/SquareGrid.vue'
+import Stretch from './components/Stretch.vue'
+import Texture from './components/Texture.vue'
+import ThreeDots from './components/ThreeDots.vue'
+import TwoCube from './components/TwoCube.vue'
+import Wave from './components/Wave.vue'
+
+export {
+  Circle,
+  Circle2,
+  Circle3,
+  Circle4,
+  Circle5,
+  Circle6,
+  Circle7,
+  Circle8,
+  Circle9,
+  Circle10,
+  Circle11,
+  CubeShadow,
+  Diamond,
+  DoubleBounce,
+  Gauge,
+  Google,
+  Hexagon,
+  HourGlass,
+  Jawn,
+  Jumper,
+  LetterCube,
+  MikePad,
+  Origami,
+  Pencil,
+  PingPong,
+  Plane,
+  RotateSquare,
+  RotateSquare2,
+  RotateSquare3,
+  RotateSquare4,
+  RotateSquare5,
+  ScaleOut,
+  Socket,
+  SpinLine,
+  SquareGrid,
+  Stretch,
+  Texture,
+  ThreeDots,
+  TwoCube,
+  Wave
+}
